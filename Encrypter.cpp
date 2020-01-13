d1a2a7be0a477472d83369701561d76c3ac47573
adw63 <adw63@cs-ssh.argo.uwf.edu>
2020-01-12 20:50:58 -0600

    There is no such thing as true random

--- Encrypter.cpp
+++ Encrypter.cpp
@@ -3,12 +3,20 @@
 Encrypter::Encrypter(std::string userID)
 {
     this->userID = userID;
-    
+    this->password = "";
+    int i = 0;
     //generate a random password full of small characters, needs to be 9 characters lowercase 
-    /*while(i < 9)
-    {
+    char character[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 
-    }*/
+    while(i < 9)
+    {
+       //random num generator  
+       int randomNum = (rand() % 25) + 0;
+       password += character[randomNum];
+       i++; 
+    }
+    std::cout << password << "\n";
+
 }
 
 std::string Encrypter::getUserID()
