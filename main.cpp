df682d994d6ecb08a080000d108cb3790278339e
adw63 <adw63@cs-ssh.argo.uwf.edu>
2020-01-25 18:02:18 -0600

    Finished

--- main.cpp
+++ main.cpp
@@ -10,8 +10,10 @@ int main()
 {
     std::ifstream file;
     fileMakerAndReader creator;
+    creator.textMaker();
     Encrypter* encrypt;
     int collisions = 0;
+    int placed =0;
 
     Stack** hashTable;
     hashTable = new Stack*[90000];
@@ -26,24 +28,26 @@ int main()
     while(i < 88799)
     {
         
-        if(i == 0)
-        {
-           encrypt = creator.objectMaker();
-           std::cout << "reached\n";
+      
+        encrypt = creator.objectMaker();
+        encrypt->encryptPassword();
+        //std::cout << "reached\n";
             
-        }
+        
         int hashKey = encrypt->getSumOfUserID() % 90000;
         
         if(hashTable[hashKey] == nullptr)
         {
             hashTable[hashKey] = new Stack;
             hashTable[hashKey]->push(encrypt);
-            std::cout << hashKey << "\n";
+            //std::cout << hashKey << "\n";
+            placed++;
         }
 
-        if(hashTable[hashKey] != nullptr)
+        else
         {
             hashTable[hashKey]->push(encrypt);
+            placed++;
             collisions++;
         }
         //std::cout << creator.objectMaker()->getUserID();
@@ -54,17 +58,58 @@ int main()
         i++;
     }
 
-    int placed =0;
+    
     int j = 0;
     while(j < 88799)
     {
         if(hashTable[j] != nullptr)
         {
-            placed++;
+            //placed++;
         }
         j++;
     }
     std::cout << "hashes placed: " << placed << "\n";
     std::cout << "collisions: " << collisions << "\n";
+
+    std::string userID = "default";
+    std::string password ="";
+   
+    while(userID != "q")
+    {
+        
+        std::cout << "Please enter UserID or q to quit: ";
+        std::cin >> userID;
+    
+        if(userID == "q") break;
+        std::cout << "Please enter password: ";
+        std::cin >> password;
+
+        Encrypter* finder = new Encrypter(userID);
+
+        finder->setPassword(password);
+        finder->encryptPassword();
+        password = finder->getPassword();
+        int sum = finder->getSumOfUserID();
+
+        int hashKey = sum % 90000;
+        
+        finder = nullptr;
+
+        if(hashTable[hashKey] != nullptr)
+        {
+            finder = hashTable[hashKey]->search(userID,password);
+        }
+        
+        if(finder == nullptr)
+        {
+            std::cout << "Not found\n";
+        }
+        else
+        {
+            std::cout << "Found!\n";
+        }
+        
+    }
+    
 }
 
